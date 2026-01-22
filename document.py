21991abbf2d7383e0a76b237cdd162d53d6dcc50
Pranjal Ambwani <PRANJAL.AMBWANI012@nmims.in>
2026-01-22 08:22:11 +0530

    Create document.py

---
+++ document.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = PyPDFLoader("docs/GreenGrow.pdf")
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+splits = text_splitter.split_documents(documents)
